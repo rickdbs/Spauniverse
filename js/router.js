b1eb26c171d01ab9ad18c74c9312ae4bc01c93e8
Henrique <rickpfss@gmail.com>
2023-03-16 18:07:13 -0300

    correção de bugs nos links

--- js/router.js
+++ js/router.js
@@ -17,10 +17,12 @@ export class Router {
     }
 
     handle(){
+        
         const { pathname } = window.location
         const route = this.routes[pathname] || this.routes[404]
 
-        fetch(route).then(data => data.text()).then(html => { document.querySelector('#app').innerHTML = html
+        fetch(route).then(data => data.text()).then(html => {
+            document.querySelector('#app').innerHTML = html
     })
 
     }
